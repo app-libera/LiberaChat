2e8c775449dd4115f93524485e9d7dabb747c5a9
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-10-07 22:36:33 +0200

    1.0.0 fixed prevent escaping

--- admin/kiwi_app_liberachat.php
+++ admin/kiwi_app_liberachat.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-   <h1>Configuring APP LiberaChat</h1>
+   <h1><?php esc_html_e('Configuring APP LiberaChat'); ?></h1>
  <form method="POST" action="options.php">
             <?php
                 settings_fields('liberachat-reg');
@@ -8,37 +8,37 @@
     <table class="form-table"> 
      <tr valign="top">
       <th scope="row">
-       <label for="liberachat_server"> Connected from:</label>
+       <label for="liberachat_server"><?php esc_html_e('Connected from: ', 'app-liberachat'); ?></label>
 	  </th>
 	 <td>	
-      <input type="text" style="width: 200px" name="liberachat_server" id="liberachat_server" value="https://web.libera.chat" disabled="1" maxlength="10" /> <a href="https://libera.chat" target="_blank" title="Libera.Chat">Libera.Chat</a> the IRC network for free & open source software
+      <input type="text" style="width: 200px" name="liberachat_server" id="liberachat_server" value="https://web.libera.chat" disabled="1" maxlength="10" /> <a href="https://libera.chat" target="_blank" title="Libera.Chat">Libera.Chat</a> <?php esc_html_e('the IRC network for free & open source software',  'app-liberachat'); ?>
      </td> 
 	 </tr> 
 	</table>
 	<table class="form-table"> 
      <tr valign="top">
       <th scope="row">
-       <label for="liberachat_nick">Nick Suggestion</label>
+       <label for="liberachat_nick"> <?php esc_html_e('Nick Suggestion: ', 'app-liberachat'); ?></label>
 	  </th>
 	 <td>                
-	  <input  type="text" style="width: 120px" name="liberachat_nick" id="liberachat_nick" value="<?php echo get_option('liberachat_nick'); ?>"/> Default nickname for your chatroom's LiberaChat. <a title="? is replaced with 3 random numbers">(Default is Guest?)</a>
+	  <input  type="text" style="width: 120px" name="liberachat_nick" id="liberachat_nick" value="<?php echo esc_attr( get_option( 'liberachat_nick' ) ); ?>"/> <?php esc_html_e('Default nickname for your chatroom`s LiberaChat. (Default is Guest?) ? is replaced with 3 random numbers',  'app-liberachat'); ?>
      </td>   
      </tr>  
     </table>	 
 	<table class="form-table"> 
 	  <tr valign="top">
 		<th scope="row">
-			<label for="liberachat_chan"> Channel to room:</label>
+			<label for="liberachat_chan"><?php esc_html_e('Channel to room:', 'app-liberachat'); ?></label>
 	    </th>
        <td>
-	    <input  type="text" style="width: 120px" name="liberachat_chan" id="liberachat_chan" value="<?php echo get_option('liberachat_chan'); ?>"/> The name of your chatroom. ( Default is #Libera )  
+	    <input  type="text" style="width: 120px" name="liberachat_chan" id="liberachat_chan" value="<?php echo esc_attr( get_option( 'liberachat_chan' ) ); ?>"/><?php esc_html_e('The name of your chatroom. Default is #Libera', 'app-liberachat'); ?>
 	   </td>	
 	  </tr>  
 	</table>
 	<table class="form-table"> 		
 	 <tr valign="top">
 	 <th scope="row">
-        <label for="liberachat_style">Select Themes:</label>
+        <label for="liberachat_style"><?php esc_html_e('Select Themes:', 'app-liberachat'); ?></label>
 	 </th>
      <td>
 	    <select name="liberachat_style" id="liberachat_style">
@@ -51,38 +51,39 @@
 			   <option value="coffee" <?php selected(get_option('liberachat_style'), "coffee"); ?>>Coffee</option>
 			   <option value="grayfox" <?php selected(get_option('liberachat_style'), "grayfox"); ?>>GrayFox</option>
         </select>
-	 Color style of the chatroom. (Default is Default)
+	 <?php esc_html_e('Color style of the chatroom. (Default is Default)', 'app-liberachat'); ?>
      </td>	
 	 </tr>  
 	</table>				
   <table class="form-table">
 	 <tr valign="top">
 		<th scope="row">
-		<label for="liberachat_width">Width:</label>
+		<label for="liberachat_width"><?php esc_html_e('Width:', 'app-liberachat'); ?></label>
 		</th>
-			<td>
+			<td>	
     <input type="text" 
 	name="liberachat_width"
 	id="liberachat_width"
-	value="<?php echo get_option('liberachat_width'); ?>" size="8">
-    Width of your chatroom. (Default is 100%)</td>
+	value="<?php echo esc_attr( get_option('liberachat_width') ); ?>" size="8">
+    <?php esc_html_e('Width of your chatroom. (Default is 100%)', 'app-liberachat'); ?></td>
     </tr>
 	<tr valign="top">
 		<th scope="row">
-			<label for="liberachat_height">Height:</label>
+			<label for="liberachat_height"><?php esc_html_e('Height:', 'app-liberachat'); ?></label>
 		</th>
 	<td>
-    <input type="text"
+    <input  type="text"
 	name="liberachat_height"
 	id="liberachat_height"
-    value="<?php echo get_option('liberachat_height'); ?>" size="8">
-    Height of your chatroom. (Default is 500)</td>		
+    value="<?php echo esc_attr( get_option('liberachat_height') ); ?>" size="8">
+    <?php esc_html_e('Height of your chatroom. (Default is 500)', 'app-liberachat'); ?></td>		
   </tr>		
 <br/>		
+	 		
   </table>					
 			
         <p style="font-weight: bold;">
-		<code>NOTE: Users' preferences will always have priority over this configuration. For example, if a user configures that a particular nickname is used and a particular channel is accessed, then that configuration will always have priority over that of that configuration, so it will enter the channel specified in the configuration.</code></p>
+		<code><?php esc_html_e('NOTE: Users` preferences will always have priority over this configuration. For example, if a user configures that a particular nickname is used and a particular channel is accessed, then that configuration will always have priority over that of that configuration, so it will enter the channel specified in the configuration.', 'app-liberachat'); ?></code></p>
             <?php submit_button(); ?>
  </form>
 </div>
