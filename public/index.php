efef808b7c66581001b6f7875b51c67f36449963
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-10-21 14:33:22 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,17 +3,13 @@ defined('ABSPATH') or die("Don't change anything here!");
 
 function liberachat_page( $atts ) {
     $url = APP_LIBERA_CHAT_URLBASE."";
-	
 	 if (get_option('liberachat_server') != 'https://web.libera.chat')
-       
         $url = $url."".get_option('liberachat_server');
 	 // ? is replaced whit 3 radom numbers 
 	 if (get_option('liberachat_nick') != '')
         $url = $url."/?nick=".str_replace("?", rand(100,999), get_option('liberachat_nick'));
-	
      if (get_option('liberachat_style') != '')
         $url = $url."&theme=".get_option('liberachat_style');
-	
 	  $channels = isset($atts['chan']) ? $atts['chan'] : '';
      if ($channels == '')
         $channels = get_option('liberachat_chan');
@@ -24,10 +20,10 @@ function liberachat_page( $atts ) {
 <center><iframe marginwidth="0" marginheight="0" src="<?php echo esc_url( $url ); ?>"
 <?php
     if (get_option('liberachat_width') != '')
-         echo esc_attr ('width'), ('='), ('"'.get_option('liberachat_width').'"'); ?>
+	echo "width=\"".esc_attr( get_option('liberachat_width'))."\""; ?>
  <?php
     if (get_option('liberachat_height') != '')
-         echo esc_attr ('height'), ('='), ('"'.get_option('liberachat_height').'"'); ?>
+        echo "height=\"".esc_attr( get_option('liberachat_height'))."\""; ?>
  scrolling="no" frameborder="0"></iframe></center>
 <?php
 }
